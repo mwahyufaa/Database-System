08a52d6b1234cf6027318c31254607003ae769a2
Muhammad Wahyu Fajrilah <muhammadwahyufajrilah@gmail.com>
2024-06-14 08:59:41 +0700

    Add files via upload

---
+++ identifiers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+    char name[50];
+    float ptkp;
+    float total_pajak;
+    float income_per_bulan;
+    float income_per_tahun;
+    float kebijakan;
+    int status;
+    int tanggungan_num;
+
+    double pkp;
